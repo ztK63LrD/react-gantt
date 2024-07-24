7d0d815fc97971748ea0472c6672ab2c90b97943
亦世凡华 <2014106247@qq.com>
2024-07-24 14:38:33 +0800

    同步

---
+++ src/components/gantt/index copy.tsx
@@ -0,0 +1,141 @@
+import { useEffect, useRef } from 'react';
+import { gantt } from 'dhtmlx-gantt';
+import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
+import './index.less'
+
+const data = {
+    // 任务数据
+    data: [
+        {
+            id: 1,
+            text: 'projectName',
+            start_date: '01-04-2023',
+            end_date: '05-12-2023',
+            duration: 48, 
+            progress: 0.3,
+        },
+        {
+            id: 2,
+            text: '任务1',
+            start_date: '02-04-2023',
+            end_date: '11-07-2023',
+            duration: 1020,
+            progress: 0.6,
+            // parent: 1
+        },
+        {
+            id: 3,
+            text: '任务21',
+            start_date: '12-07-2023',
+            end_date: '09-09-2023',
+            duration: 0, 
+            progress: 0,
+            // parent: 1
+        }
+    ],
+    // 任务连线数据
+    links: [
+        { id: 1, source: 1, target: 2, type: '1' },
+        { id: 2, source: 2, target: 3, type: '0' }
+    ]
+};
+
+// 左侧标题数据
+const columns = [
+    { name: 'text', label: '项目名称',  width: 100, align: "center" },
+    { name: 'start_date', label: '开始时间', width: 100, align: "center" },
+    { name: 'start_date', label: '开始时间', width: 100, align: "center" },
+    { name: 'duration', label: '计划工期', width: 100, align: "center" },
+];
+
+const GanttView = () => {
+    // 获取gantrt容器实例
+    const ganttRef = useRef<any>();
+    // 初始化gantt
+    const initGantt = () => {
+        // 基础配置
+        gantt.clearAll() // 清空之前的配置
+        gantt.i18n.setLocale('cn'); // 设置中文
+        gantt.config.readonly = true; // 设置为只读，否则是可以移动甘特图和连线的
+        gantt.init(ganttRef.current); // 初始化甘特图
+        gantt.parse(data); // 渲染数据
+
+        // 甘特图样式设置
+        gantt.config.scale_height = 60; // 设置表头高度
+        // 设置头部左侧表头标题背景颜色
+        gantt.templates.grid_header_class = function (date, scale) {
+            return "gantt_grid_head111";
+        };
+        // 设置左侧标题表内容背景颜色
+        gantt.templates.grid_row_class = function (date, scale) {
+            return "gantt_scale_cell111";
+        };
+        // 设置头部右侧上标题内容背景颜色
+        gantt.templates.scale_cell_class = function (scale) {
+            return "gantt_grid_head111";
+        };
+        // 设置头部右侧下标题内容背景颜色
+        gantt.templates.scale_row_class = function (scale) {
+            return "gantt_grid_head111";
+        };
+        // 设置表主内容背景颜色
+        gantt.templates.task_row_class = function (start, end, task) {
+            return "gantt_task111";
+        };
+        gantt.config.sort = true; // 设置点击左侧表头可排序
+        gantt.config.columns = columns; // 设置左侧表头数据
+        gantt.config.scales = [ // 设置表头右侧刻度
+            // 设置时间刻度相关属性
+            // 显示月日用这个
+            // { unit: 'month', step: 1, format: '%Y-%m' },
+            // { unit: 'day', step: 1, format: '%Y-%m-%d' }
+            // 显示年月用这个
+            { unit: 'year', step: 1, format: '%Y' },
+            { unit: 'month', step: 1, format: '%M' }
+        ];
+
+        // 表内容样式设置
+        gantt.config.row_height = 40; // 设置内容行高
+        gantt.config.bar_height = 40; // 设置进度条高度
+        gantt.templates.task_text = function (start, end, task) { // 自定义内容进度上的文本
+            return '内容'
+        };
+
+        // tooltips样式设置
+        gantt.plugins({
+            tooltip: true,
+            // quick_info: true, // 快速信息框
+            // multiselect: true,// 激活多任务选择
+        });
+        gantt.config.tooltip_offset_x = 10; // 设置tooltips水平偏移量
+        gantt.config.tooltip_offset_y = 30; // 设置tooltips垂直偏移量
+        gantt.templates.tooltip_text = function (start, end, task) {
+            return (
+                task.text +
+                '<br/><span>开始:</span> ' +
+                gantt.templates.tooltip_date_format(start) +
+                '<br/><span>结束:</span> ' +
+                gantt.templates.tooltip_date_format(end) +
+                '<br/><span>进度:</span> ' +
+                // Math.round(task.progress * 100) +
+                '%'
+            );
+        };
+
+        // 设置连线事件
+        gantt.config.show_links = true;
+        gantt.config.drag_project = true;
+    }
+
+    useEffect(() => {
+        initGantt();
+    }, []);
+
+    return (
+        <div className="ganttView">
+            <div className='ganttContainer' ref={ganttRef}></div>
+        </div>
+    )
+};
+export default GanttView;
+
--- src/components/gantt/index.tsx
+++ src/components/gantt/index.tsx
@@ -6,47 +6,27 @@ import './index.less'
 const data = {
     // 任务数据
     data: [
-        {
-            id: 1,
-            text: 'projectName',
-            start_date: '01-04-2023',
-            end_date: '05-12-2023',
-            duration: 148, 
-            progress: 0.3,
-            color: '#b38989'
-        },
-        {
-            id: 2,
-            text: '任务1',
-            start_date: '02-04-2023',
-            end_date: '11-07-2023',
-            duration: 1020,
-            progress: 0.6,
-            // parent: 1
-        },
-        {
-            id: 3,
-            text: '任务2',
-            start_date: '12-07-2023',
-            end_date: '09-09-2023',
-            duration: 519,
-            progress: 0,
-            // parent: 1
-        }
+        //第一组 整条数据需要带上render属性  里面多段的数据parent执行整条的id
+        { id: 1, name: '张三', render: 'split',  text: '' },
+        { id: 100, parent: 1, text: '派工', color: '#008c8c', start_date: '15-06-2024 08:30', end_date: '15-06-2024: 10:30' },
+        { id: 101, parent: 1, text: '休息', color: 'blue', start_date: '16-06-2024: 13:00', end_date: '16-06-2024:23:00' },
+        
+        // 第二组
+        { id: 2, name: '李四', render: 'split', text: '' },
+        { id: 102, parent: 2, text: '派工', color: '#008c8c', start_date: '15-06-2024 18:30', end_date: '15-06-2024: 22:30' },
+        { id: 103, parent: 2, text: '休息', color: 'blue', start_date: '16-06-2024: 13:00', end_date: '16-06-2024:23:00' },
+
+        // 第三组
+        { id: 3, name: '王五', render: 'split', text: '' },
+        { id: 104, parent: 3, text: '派工', color: '#008c8c', start_date: '15-06-2024 8:30', end_date: '15-06-2024: 22:30' },
+        { id: 105, parent: 3, text: '休息', color: 'blue', start_date: '15-06-2024: 13:00', end_date: '16-06-2024:23:00' },
     ],
-    // 任务连线数据
-    links: [
-        { id: 1, source: 1, target: 2, type: '1' },
-        { id: 2, source: 2, target: 3, type: '0' }
-    ]
 };
 
 // 左侧标题数据
 const columns = [
-    { name: 'text', label: '项目名称',  width: 100, align: "center" },
-    { name: 'start_date', label: '开始时间', width: 100, align: "center" },
-    { name: 'start_date', label: '开始时间', width: 100, align: "center" },
-    { name: 'duration', label: '计划工期', width: 100, align: "center" },
+    { name: 'id', label: '序号', resize: true,  max_width: 60, align: "center" },
+    { name: 'name', label: '姓名', resize: true, max_width: 60, align: "center" },
 ];
 
 const GanttView = () => {
@@ -54,60 +34,40 @@ const GanttView = () => {
     const ganttRef = useRef<any>();
     // 初始化gantt
     const initGantt = () => {
-        // 基础配置
         gantt.clearAll() // 清空之前的配置
         gantt.i18n.setLocale('cn'); // 设置中文
-        gantt.config.readonly = true; // 设置为只读
+        gantt.config.readonly = true; // 设置为只读，否则是可以移动甘特图和连线的
+        gantt.config.start_date = new Date(2024, 5, 15); // 设置甘特图开始日期  
+        gantt.config.end_date = new Date(2024, 5, 18); // 设置甘特图结束日期  
+        gantt.init(ganttRef.current); // 初始化甘特图
+        gantt.parse(data); // 渲染数据
 
-        // 表头样式设置
+        gantt.config.columns = columns; // 设置左侧表头数据
         gantt.config.scale_height = 60; // 设置表头高度
-        gantt.config.sort = true; // 点击表头可排序
-        gantt.config.columns = columns; // 设置表头
-        gantt.config.scales = [ // 设置表头右侧刻度
-            // 设置时间刻度相关属性
-            // 显示月日用这个
-            // { unit: 'month', step: 1, format: '%Y-%m' },
-            // { unit: 'day', step: 1, format: '%Y-%m-%d' }
-            // 显示年月用这个
-            { unit: 'year', step: 1, format: '%Y' },
-            { unit: 'month', step: 1, format: '%M' }
+        gantt.config.min_column_width = 10; // 设置列最小宽度
+        // 设置头部右侧上标题内容背景颜色
+        gantt.templates.scale_cell_class = function (scale) {
+            return "gantt_grid_head_top";
+        };
+
+
+        gantt.config.scales = [ // 设置甘特图时间轴
+            { unit: 'day', step: 1, format: '%Y/%m/%d' },
+            { unit: 'hour', step: 1, format: function(date) {  
+                return date.getHours(); // 显示从0到23的小时范围  
+            }}  
         ];
 
         // 表内容样式设置
+        gantt.templates.task_row_class = function (start, end, task) { // 设置表主内容背景颜色
+            return "gantt_task_main_content";
+        };
         gantt.config.row_height = 40; // 设置内容行高
         gantt.config.bar_height = 40; // 设置进度条高度
-        gantt.templates.task_text = function (start, end, task) { // 自定义内容进度上的文本
-            return '内容'
+        gantt.templates.task_text = function (start, end, task) {
+            return `<div style="color: #fff; font-size: 14px;">${task?.text}</div>`;
         };
 
-        // tooltips样式设置
-        gantt.plugins({
-            tooltip: true,
-            // quick_info: true, // 快速信息框
-            // multiselect: true,// 激活多任务选择
-        });
-        gantt.config.tooltip_offset_x = 10; 
-        gantt.config.tooltip_offset_y = 30;
-        gantt.templates.tooltip_text = function (start, end, task) {
-            return (
-                task.text +
-                '<br/><span>开始:</span> ' +
-                gantt.templates.tooltip_date_format(start) +
-                '<br/><span>结束:</span> ' +
-                gantt.templates.tooltip_date_format(end) +
-                '<br/><span>进度:</span> ' +
-                // Math.round(task.progress * 100) +
-                '%'
-            );
-        };
-
-        // 设置连线事件
-        gantt.config.show_links = true;
-
-        // 初始化甘特图
-        gantt.init(ganttRef.current);
-        // 渲染数据
-        gantt.parse(data);
     }
 
     useEffect(() => {
@@ -115,8 +75,8 @@ const GanttView = () => {
     }, []);
 
     return (
-        <div className="ganttView">
-            <div className='ganttContainer' ref={ganttRef}></div>
+        <div className="ganttView"  style={{ width: '100%', overflow: 'hidden' }}>
+            <div className='ganttContainer' ref={ganttRef} style={{ width: '100%' }}></div>
         </div>
     )
 };
